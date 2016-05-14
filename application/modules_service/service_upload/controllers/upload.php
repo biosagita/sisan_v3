a3a5cadb120e819ca1314e821d43b7a386379cb8
andy <juliadi@femaledaily.com>
2016-05-14 16:51:39 +0700

    update service upload file

--- application/modules_service/service_upload/controllers/upload.php
+++ application/modules_service/service_upload/controllers/upload.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Upload extends MY_Service {
+class Upload extends MY_Controller {
 	
 	function __construct(){
 		parent::__construct();
