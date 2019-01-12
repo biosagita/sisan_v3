447184c440337b86ac6c2b93785ee850337fb4dd
andysociolla <andy.juliadi@sociolla.com>
2019-01-12 20:39:05 +0700

    add master permasalahan

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -187,6 +187,11 @@ class MY_Admin extends MY_Controller {
                                 'url_link' => site_url('backend_visitors/visitors/page_content_ajax'),
                                 'icon' => '<i class="glyph-icon icon-linecons-diamond"></i>',
                             ),
+                            array(
+                                'label' => 'Permasalahan',
+                                'url_link' => site_url('backend_permasalahan/permasalahan/page_content_ajax'),
+                                'icon' => '<i class="glyph-icon icon-linecons-diamond"></i>',
+                            ),
 						),
 					),
 					array(
